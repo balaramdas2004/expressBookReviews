5a5f49d68d02ff7868739a93d45ab132ef0b1555
Balaram Das <bd9650576@gmail.com>
2026-01-11 15:50:41 +0530

    Refactor book retrieval to use Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,57 +1,75 @@
 const express = require('express');
 const axios = require('axios');
-let books = require("./booksdb.js");
 const public_users = express.Router();
 
-// Get all books (ASYNC / AWAIT)
+// Base URL for books data
+const BOOKS_API = "http://localhost:3000";
+
+// Get all books (ASYNC / AWAIT + AXIOS)
 public_users.get('/', async (req, res) => {
   try {
-    return res.status(200).json(books);
+    const response = await axios.get(`${BOOKS_API}/books`);
+    return res.status(200).json(response.data);
   } catch (error) {
-    return res.status(500).json({ message: "Error retrieving books" });
+    return res.status(500).json({ message: "Failed to fetch books" });
   }
 });
 
-// Get book by ISBN (ASYNC / AWAIT)
+// Get book by ISBN (ASYNC / AWAIT + AXIOS)
 public_users.get('/isbn/:isbn', async (req, res) => {
-  const isbn = req.params.isbn;
   try {
-    if (books[isbn]) {
-      return res.status(200).json(books[isbn]);
+    const response = await axios.get(`${BOOKS_API}/books`);
+    const book = response.data[req.params.isbn];
+
+    if (book) {
+      return res.status(200).json(book);
     } else {
       return res.status(404).json({ message: "Book not found" });
     }
   } catch (error) {
-    return res.status(500).json({ message: "Error retrieving book" });
+    return res.status(500).json({ message: "Error retrieving book by ISBN" });
   }
 });
 
-// Get books by Author (PROMISE)
+// Get books by Author (PROMISE CALLBACK + AXIOS)
 public_users.get('/author/:author', (req, res) => {
-  const author = req.params.author;
-  let result = {};
+  axios.get(`${BOOKS_API}/books`)
+    .then(response => {
+      const books = response.data;
+      const result = {};
 
-  Object.keys(books).forEach(key => {
-    if (books[key].author === author) {
-      result[key] = books[key];
-    }
-  });
+      Object.keys(books).forEach(key => {
+        if (books[key].author === req.params.author) {
+          result[key] = books[key];
+        }
+      });
 
-  return res.status(200).json(result);
+      res.status(200).json(result);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Error retrieving books by author" });
+    });
 });
 
-// Get books by Title (PROMISE)
+// Get books by Title (PROMISE CALLBACK + AXIOS)
 public_users.get('/title/:title', (req, res) => {
-  const title = req.params.title;
-  let result = {};
+  axios.get(`${BOOKS_API}/books`)
+    .then(response => {
+      const books = response.data;
+      const result = {};
 
-  Object.keys(books).forEach(key => {
-    if (books[key].title === title) {
-      result[key] = books[key];
-    }
-  });
+      Object.keys(books).forEach(key => {
+        if (books[key].title === req.params.title) {
+          result[key] = books[key];
+        }
+      });
 
-  return res.status(200).json(result);
+      res.status(200).json(result);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Error retrieving books by title" });
+    });
 });
 
 module.exports = public_users;
+
